092cbb0b9397cc79c581c752fe3d49a7c6d41c47
Harry Moreno <morenoh149@gmail.com>
2015-12-03 15:43:48 -0500

    factor out functions into the Terminal Prototype

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 function Terminal() {
   this.total = 0
-  var pricing = [{
+  this._pricing = [{
     name: 'productName',
     price: {
       unitPrice: 2.25,
@@ -12,59 +12,59 @@ function Terminal() {
     },
     quantity: 0 // used internally
   }]
+}
 
-  /*
-   * Adds new pricing scheme to our pricing list
-   * expects an object of the form:
-   * { name: string,
-   *   price: { unitPrice: float, volume: { units: float, price: float }}}
-   * names must be unique
-   */
-   this.setPricing = function(obj) {
-     if (obj.name && obj.price && obj.price.unitPrice) {
-       if (!pricing.some(function(p) { return p.name === obj.name })) {
-         if (!obj.quantity) {
-           obj.quantity = 0
-         }
-         pricing.push(obj)
-       } else {
-         throw 'A product with that name already exists.'
-       }
-     } else {
-       throw 'Invalid price.'
-     }
-   }
+/*
+ * Private function, used internally to calculate cart total
+ */
+Terminal.prototype._sum = function() {
+  var result = 0
+  this._pricing.forEach(function(p) {
+    if (p.price.volume) {
+      var whole = Math.floor(p.quantity/p.price.volume.units)
+      var remainder = p.quantity - (whole * p.price.volume.units)
+      var cost = (whole * p.price.volume.price) + (remainder * p.price.unitPrice)
+    } else {
+      var cost = p.price.unitPrice * p.quantity
+    }
+    result += cost
+  })
+  return result
+}
 
-   /*
-    * Adds item to cart,
-    * updates cart total
-    */
-    this.scan = function(productName) {
-     pricing.forEach(function(p) {
-       if (p.name === productName) {
-         p.quantity += 1
-         this.total = sum()
-       }
-     }, this)
-   }
+/*
+ * Adds new pricing scheme to our pricing list
+ * expects an object of the form:
+ * { name: string,
+ *   price: { unitPrice: float, volume: { units: float, price: float }}}
+ * names must be unique
+ */
+Terminal.prototype.setPricing = function(obj) {
+  if (obj.name && obj.price && obj.price.unitPrice) {
+    if (!this._pricing.some(function(p) { return p.name === obj.name })) {
+      if (!obj.quantity) {
+        obj.quantity = 0
+      }
+      this._pricing.push(obj)
+    } else {
+      throw 'A product with that name already exists.'
+    }
+  } else  {
+    throw 'Invalid price.'
+  }
+}
 
-   /*
-    * Private function, used internally to calculate cart total
-    */
-   var sum = function() {
-     var result = 0
-     pricing.forEach(function(p) {
-       if (p.price.volume) {
-         var whole = Math.floor(p.quantity/p.price.volume.units)
-         var remainder = p.quantity - (whole * p.price.volume.units)
-         var cost = (whole * p.price.volume.price) + (remainder * p.price.unitPrice)
-       } else {
-         var cost = p.price.unitPrice * p.quantity
-       }
-       result += cost
-     })
-     return result
-   }
+/*
+ * Adds item to cart,
+ * updates cart total
+ */
+Terminal.prototype.scan = function(productName) {
+  this._pricing.forEach(function(p) {
+    if (p.name === productName) {
+      p.quantity += 1
+      this.total = this._sum()
+    }
+  }, this)
 }
 
 module.exports = Terminal
